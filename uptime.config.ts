d4dbce3e685779a9496dfcc8842ad697fb6f8461
ifAiGangZai <164759698+ifAiGangZai@users.noreply.github.com>
2024-05-26 12:02:30 +0000

    chore:增加页面配置文件

---
+++ uptime.config.ts
@@ -0,0 +1,7 @@
+const pageConfig = {
+    title: "iFAI's Status Page",
+    links: [
+      { link: 'https://news.ifai.io/zh', label: 'GitHub' },
+      { link: 'ifai@ifai.io', label: 'Email Me', highlight: true },
+    ],
+  }
